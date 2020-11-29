d60159c70e2fac6f5af495bfd7df87899f90d0f0
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2020-11-29 11:29:44 -0500

    added css dependencies for maps

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -15,6 +15,9 @@ import 'font-awesome/css/font-awesome.min.css';
 import '../styles/styles.scss';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-routing-machine/dist/leaflet-routing-machine.css'
+import 'esri-leaflet-geocoder/dist/esri-leaflet-geocoder.css'
+import 'leaflet-geosearch/assets/css/leaflet.css'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
