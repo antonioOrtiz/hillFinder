bd829ddb9d43a1799c30babc5affb7133a8e9823
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-07-09 10:37:52 -0400

    added @fullhuman/postcss-purgecss

--- posstcss.config.js
+++ posstcss.config.js
@@ -8,7 +8,8 @@ module.exports = {
               './pages/**/*.{js,jsx,ts,tsx}',
               './components/**/*.{js,jsx,ts,tsx}'
             ],
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+            safelist: ['html', 'body']
           },
           'postcss-flexbugs-fixes',
           [
