a4329559733d8f7207cef74cecefa251ed7ed708
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-07-09 10:42:48 -0400

    added @fullhuman/postcss-purgecss

--- posstcss.config.js
+++ posstcss.config.js
@@ -1,31 +1,23 @@
 module.exports = {
-  plugins:
-    process.env.NODE_ENV === 'production'
-      ? [
-          '@fullhuman/postcss-purgecss',
-          {
-            content: [
-              './pages/**/*.{js,jsx,ts,tsx}',
-              './components/**/*.{js,jsx,ts,tsx}'
-            ],
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-            safelist: ['html', 'body']
-          },
-          'postcss-flexbugs-fixes',
-          [
-            'postcss-preset-env',
-            {
-              autoprefixer: {
-                flexbox: 'no-2009'
-              },
-              stage: 3,
-              features: {
-                'custom-properties': false
-              }
-            }
-          ]
-        ]
-      : [
-          // No transformations in development
-        ]
+  plugins: [
+    '@fullhuman/postcss-purgecss',
+    {
+      content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+      safelist: ['html', 'body']
+    },
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009'
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false
+        }
+      }
+    ]
+  ]
 };
