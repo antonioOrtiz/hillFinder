9e83a0304ad11a8bb0d5b1cd62722882b0603cfe
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-07-10 19:37:59 -0400

    trying postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production'
+      ? {
+          '@fullhuman/postcss-purgecss': {
+            // added sections folder and changed extension to jsx
+            content: ['./components/**/*.jsx', './pages/**/*.jsx'],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+          }
+        }
+      : {})
+  }
+};
