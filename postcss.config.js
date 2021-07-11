60297795f41dcbc004f68b1cc81120a17e60aafa
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-07-10 20:06:22 -0400

    trying to fix Error: PostCSS plugin postcss-purgecss requires PostCSS 8.

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,8 @@
 module.exports = {
-  plugins: [
-    [
-      '@fullhuman/postcss-purgecss',
-      {
-        content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
-        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-      }
-    ],
-    'postcss-preset-env'
-  ]
+  plugins: {
+    '@fullhuman/postcss-purgecss': {
+      content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    }
+  }
 };
