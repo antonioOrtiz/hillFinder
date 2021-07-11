0b3969491384593169735155774e82fb1844f321
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-07-11 16:07:08 -0400

    added comments to styles.css/scss to protect styles

--- postcss.config.js
+++ postcss.config.js
@@ -17,8 +17,7 @@ module.exports = {
       '@fullhuman/postcss-purgecss',
       {
         content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
-        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-        safelist: ['html', 'body']
+        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
       }
     ]
   ]
