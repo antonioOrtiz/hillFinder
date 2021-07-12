29fce2c3f503a7c265f5b1443b5333bdbf6a913f
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-07-12 09:38:16 -0400

    trying to get purgecss to work

--- postcss.config.js
+++ postcss.config.js
@@ -17,7 +17,8 @@ module.exports = {
       '@fullhuman/postcss-purgecss',
       {
         content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
-        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+        safelist: ['html', 'body']
       }
     ]
   ]
